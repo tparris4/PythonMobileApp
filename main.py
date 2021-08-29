35b76c808031ff93cde7e8f9d4576d9e334b8532
typar <typarris421@gmail.com>
2021-08-29 18:38:43 -0400

    added txt files

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+import glob
+from datetime import datetime
+from pathlib import Path
 # from kivy.uix.gridlayout import GridLayout
 # dir(GridLayout)
 Builder.load_file('design.kv')
+
+# loginscreen is referring to login screen in kv file
+# we access it through login screen class
+# self is pointing to login screen class
 
 
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] = pword:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong.text = "Wrong username or password"
+
 
 # template for every app screen
 
@@ -18,7 +35,36 @@ class RootWidget(ScreenManager):
 
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            user = json.load(file)
+        print(users)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strttime("%Y-%m-%d-%H-%M-%S")}
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        self.manager.current = "sign_up_screen_success"
+
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.globa("quotes/*txt")
+        print(available_feelings)
+        # extract names of the files
 
 
 class MainApp(App):
